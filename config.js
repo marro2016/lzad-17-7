b7182e5722fd1fd15519a89bd1ccd7112a012365
Marcin Różański <rozanskimarcin2016@gmail.com>
2018-06-05 23:51:35 +0200

    Add

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+	'GOOGLE_CLIENT_ID':'828530776758-icuscc2eu41hso7fup73vmb7tbp364mf.apps.googleusercontent.com',
+	'GOOGLE_CLIENT_SECRET':'myGGzbtGzLCn8MEY7v6L_nC2',
+	'CALLBACK_URL':'http://localhost:3000/auth/google/callback'
+};
